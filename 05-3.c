e38919eaeb3a0de1a0bebf05277b8392db318527
bobstwwo <bobstwwo@gmail.com>
2021-02-15 16:38:10 +0300

    Lab 5 done.

--- 05-3.c
+++ 05-3.c
@@ -3,65 +3,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void reverse(char* str, size_t sz);
+
 int main()
 {
-  int     fd[2], result;
+    int pipe_parent[2], pipe_child[2], result;
+    size_t size;
+    char resstring[14];
 
-  size_t size;
-  char  resstring[14];
-
-  if (pipe(fd) < 0) {
-    printf("Can\'t open pipe\n");
-    exit(-1);
-  }
-
-  result = fork();
-
-  if (result < 0) {
-    printf("Can\'t fork child\n");
-    exit(-1);
-  } else if (result > 0) {
-
-   /* Parent process */
-
-    if (close(fd[0]) < 0) {
-      printf("parent: Can\'t close reading side of pipe\n"); exit(-1);
+    if (pipe(pipe_parent) < 0) {
+        printf("Can\'t create parent pipe\n");
+        exit(-1);
     }
 
-    size = write(fd[1], "Hello, world!", 14);
-
-    if (size != 14) {
-      printf("Can\'t write all string to pipe\n");
-      exit(-1);
+    if (pipe(pipe_child) < 0) {
+        printf("Can\'t create child pipe\n");
+        exit(-1);
     }
 
-    if (close(fd[1]) < 0) {
-      printf("parent: Can\'t close writing side of pipe\n"); exit(-1);
+    result = fork();
+    if (result < 0) {
+        printf("Can\'t fork child\n");
+        exit(-1);
+    } else if (result > 0) {
+        // parent
+        close(pipe_parent[0]);
+        close(pipe_child[1]);
+        size = write(pipe_parent[1], "Hello, world!", 14);
+        if (size != 14) {
+            printf("Can\'t write all string\n");
+            exit(-1);
+        }
+        close(pipe_parent[1]);
+
+        size = read(pipe_child[0], resstring, 14);
+        if (size != 14) {
+            printf("Can\'t read from child\n");
+            exit(-1);
+        }
+        printf("Parent received message from child: %s\n", resstring);
+        printf("Parent exit\n");
+    } else {
+        close(pipe_parent[1]);
+        close(pipe_child[0]);
+        size = read(pipe_parent[0], resstring, 14);
+        if (size < 0) {
+            printf("Can\'t get from parent\n");
+            exit(-1);
+        }
+        printf("Message from parent: %s\n", resstring);
+        reverse(resstring, 13);
+        size = write(pipe_child[1], resstring, 14);
+        close(pipe_parent[0]);
+        printf("Exit child\n");
     }
+    return 0;
+}
 
-    printf("Parent exit\n");
-
-  } else {
-
-    /* Child process */
-
-    if (close(fd[1]) < 0) {
-      printf("child: Can\'t close writing side of pipe\n"); exit(-1);
+void reverse(char* str, size_t sz)
+{
+    for (int i = 0; i < sz / 2; i++) {
+        char t = str[i];
+        str[i] = str[sz - i - 1];
+        str[sz - i - 1] = t;
     }
-
-    size = read(fd[0], resstring, 14);
-
-    if (size < 0) {
-      printf("Can\'t read string from pipe\n");
-      exit(-1);
-    }
-
-    printf("Child exit, resstring:%s\n", resstring);
-
-    if (close(fd[0]) < 0) {
-      printf("child: Can\'t close reading side of pipe\n"); exit(-1);
-    }
-  }
-
-  return 0;
 }
