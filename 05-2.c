f9e9e52e60b45149ac4721ae3c8ec8dc6d1a41a4
bobstwwo <bobstwwo@gmail.com>
2021-02-22 17:03:26 +0300

    Lab 6 done.

--- 05-2.c
+++ 05-2.c
@@ -1,44 +1,26 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <signal.h>
+#include <fcntl.h>
 
-int main()
-{
-  int     fd[2];
-  ssize_t size;
-  char     string[] = "Hello, world!";
-  char  resstring[14];
-  //
-  // Try to create a pipe
-  //
-  if (pipe(fd) < 0) {
-    printf("Can\'t open pipe\n");
-    exit(-1);
-  }
+int result = 0;
 
-  size = write(fd[1], string, 14);
+int main() {
+	int fd[2];
+	size_t size = 1;
+	char* resstring = malloc(1 * sizeof(char));
 
-  if (size != 14) {
-    printf("Can\'t write all string to pipe\n");
-    exit(-1);
-  }
+	if(pipe(fd) < 0) {
+		printf("Can\'t create pipe\n");
+		exit(-1);
+	}
+	fcntl(fd[1], F_SETFL, fcntl(fd[1], F_GETFL) | O_NONBLOCK);
 
-  size = read(fd[0], resstring, 14);
-
-  if (size < 0) {
-    printf("Can\'t read string from pipe\n");
-    exit(-1);
-  }
-
-  printf("%s\n", resstring);
-
-  if (close(fd[0]) < 0) {
-    printf("Can'\t close reading side of pipe\n"); exit(-1);
-  }
-  if (close(fd[1]) < 0) {
-    printf("Can'\t close writing side of  pipe\n"); exit(-1);
-  }
-
-  return 0;
+	while (size == 1) {
+		size = write(fd[1], resstring, 1);
+		result++;
+	}
+	printf("Result: %d bytes\n", result);
+	return 0;
 }
