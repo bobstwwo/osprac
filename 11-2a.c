6e982a0a948393c74098b940ccd3cbb90ebc468f
bobstwwo <bobstwwo@gmail.com>
2021-04-26 16:33:31 +0300

    Lab11 done

--- 11-2a.c
+++ 11-2a.c
@@ -10,10 +10,10 @@
 int main(void)
 {
   int msqid;      // IPC descriptor for the message queue
-  char pathname[]="11-1a.c"; // The file name used to generate the key.
+  char pathname[]="11-2a.c"; // The file name used to generate the key.
                              // A file with this name must exist in the current directory.
   key_t  key;     // IPC key
-  int i,len;      // Cycle counter and the length of the informative part of the message
+  int i,len, maxlen;      // Cycle counter and the length of the informative part of the message
 
   struct mymsgbuf // Custom structure for the message
   {
@@ -40,20 +40,13 @@ int main(void)
 
   /* Send information */
 
-  for (i = 1; i <= 5; i++) {
-    //
-    // Fill in the structure for the message and
-    // determine the length of the informative part.
-    //
-    mybuf.mtype = i;
-    mybuf.info.sinfo = i;
-    mybuf.info.finfo = i/100.0;
-    //strcpy(mybuf.mtext, "This is text message");
+  for (i = 0; i < 5; i++) {
+    
+    mybuf.mtype = 1; //тип = 1
+    mybuf.info.sinfo = i*2;
+    mybuf.info.finfo = i*2/100.0;
     len = sizeof(mybuf.info);
-    //
-    // Send the message. If there is an error,
-    // report it and delete the message queue from the system.
-    //
+    
     if (msgsnd(msqid, &mybuf, len, 0) < 0) {
       printf("Can\'t send message to queue\n");
       msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
@@ -61,16 +54,25 @@ int main(void)
     }
   }
 
-  /* Send the last message */
+  
+  printf("First program sent all the messages it wanted.\n");
 
-  mybuf.mtype = LAST_MESSAGE;
-  len         = 0;
 
-  if (msgsnd(msqid, &mybuf, len, 0) < 0) {
-    printf("Can\'t send message to queue\n");
-    msgctl(msqid, IPC_RMID, (struct msqid_ds *) NULL);
-    exit(-1);
+//GET MESSAGES
+  for(i = 0; i < 5; i++) {
+   
+    maxlen = sizeof(mybuf.info);
+//принимаем тип 2
+    if (( len = msgrcv(msqid, &mybuf, maxlen, 2, 0)) < 0) {
+      printf("Can\'t receive message from queue\n");
+      exit(-1);
+    }
+
+    printf("First program got from second: message type = %ld, sinfo = %i, finfo = %f\n",
+ mybuf.mtype, mybuf.info.sinfo, mybuf.info.finfo);
   }
+  
+  printf("First program got all the messages.\n");
 
   return 0;
 }
